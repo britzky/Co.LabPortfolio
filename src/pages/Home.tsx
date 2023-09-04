dd2e884d80976dbe881208024552ad186b9c6ae4
John <johnclapper89@gmail.com>
2023-09-04 16:08:43 -0400

    added tailwind to project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,10 @@
+import me from '../assets/me.jpg'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <img src={me} alt="Smiling man" />
+        <h1>Full Stack Software Engineer</h1>
+    </div>
   )
 }
