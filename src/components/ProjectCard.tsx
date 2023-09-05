c56820d524990ecf2a1309b1294123f67a3e5d75
John <johnclapper89@gmail.com>
2023-09-05 14:50:37 -0400

    Finsihed projects section

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,17 +3,37 @@ interface ProjectCardProps {
     title: string;
     image: string;
     description: string;
+    live: string;
+    github: string;
 }
 
-export const ProjectCard: React.FC<ProjectCardProps> = ({title, image, description}) => {
+export const ProjectCard: React.FC<ProjectCardProps> = ({title, image, description, live, github}) => {
   return (
-    <section>
-        <div>
-            <h1>{title}</h1>
+    <section className="border rounded-md p-3">
+        <div className="py-3">
+            <h1 className="text-xl underline py-3">{title}:</h1>
             <p>{description}</p>
         </div>
         <div>
             <img src={image} alt="Project image" />
+        </div>
+        <div className="flex justify-around mt-5">
+            <a 
+                href={live}
+                rel="noreferrer"
+                target="_blank"
+                className="bg-sky-400 hover:bg-sky-600 text-white font-bold py-2 px-4 rounded"
+            >
+                See Live
+            </a>
+            <a
+                rel="noreferrer"
+                target="_blank"
+                className="bg-amber-400 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded"
+                href={github}
+            >
+                Source Code
+            </a>
         </div>
     </section>
   )
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,10 +1,53 @@
+import taskManager from '../assets/taskManager.png'
+import cinemaCenter from '../assets/cinema-center.png'
+import britzkysDungeon from '../assets/expandedCharactersImage.png'
+import weatherReport from '../assets/weatherImage.png'
 import { ProjectCard } from "../components"
 
 export const Projects = () => {
   return (
     <main>
-      <div>
-        <h1>Projects</h1>
+      <div className="flex flex-col items-center gap-5 py-7 ">
+        <h1 className="text-2xl font-bold">Projects</h1>
+        <ProjectCard
+          title="Weather Report"
+          description="Access current high, low, forecast, and humidity based on zipcode input.
+          Built with HTML, CSS, JavaScript, and openweathermap.org API.
+          Asynchronous operations for seamless data fetching, dynamic background images, and intuitive UI."
+          image={weatherReport}
+          live="https://www.loom.com/share/87f0225171204086a074e6967479abd2"
+          github="https://github.com/britzky/WeatherReport"
+          />
+        <ProjectCard
+          title="Task Manager"
+          description="Efficiently manage daily tasks, projects, and one-time events. 
+          Offers theme cusomization, local storage, an intuitive UI, and responsive design.
+          Build with JavaScript, React, HTML, CSS, and JSX. Provides seamless task management and personalization across devices."
+          image={taskManager}
+          live="https://britzkys-task-manager.netlify.app"
+          github="https://github.com/britzky/task-manager"
+          />
+        <ProjectCard
+          title="Cinema Center"
+          description="An immersive movie browsing experience made for movie lovers. 
+          Showcases various movie lists including Now Playing, Popular, Top Rated, and Upcoming movies. 
+          Equipped with a detailed page for every movie, search functionality, dynamic routing, and a dark mode toggle. 
+          Constructed using JavaScript, React, Tailwind CSS, Flowbite, React Router Dom, and TMDB API. 
+          Featuring an easy-to-navigate UI, responsive design, and personalized browsing experience regardless of the device"
+          image={cinemaCenter} 
+          live="https://cinema-center.netlify.app/"
+          github="https://github.com/britzky/cinema-center"
+          />
+        <ProjectCard
+          title="Britzky's Dungeon"
+          description=" Interactive Dungeons and Dragons Character Builder.
+          Create & save custom characters with race, class, alignment, and background.
+          Built with React, Material UI, and Firebase.
+          API integration for data, full CRUD operations, and interactive character cards."
+          image={britzkysDungeon}
+          live="https://gilded-squirrel-0bbe73.netlify.app"
+          github="https://github.com/britzky/React_Character_Selector"
+          />
       </div>
     </main>
   )
