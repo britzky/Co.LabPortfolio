aecc01df0e2b7ded85c4b60879373adb7d2701b8
John <johnclapper89@gmail.com>
2023-09-05 19:35:47 -0400

    cleaned up some whitespace

--- src/pages/Joke.tsx
+++ src/pages/Joke.tsx
@@ -44,12 +44,10 @@ export const Joke = () => {
         <div className="flex flex-col gap-2 py-7 items-center">
             <h1 className="text-2xl font-bold">Random Chuck Norris joke</h1>
             <p>I chose this as my API because there are a ton of Chuck Norris jokes I dont know yet.</p>
-    
             <img src={chuck} alt="Chuck Norris" className="rounded-lg" />
             {joke && <p>{joke}</p>}
             <button onClick={getJoke} type="button" className="focus:outline-none text-white bg-pink-400 hover:bg-pink-600 focus:ring-4 focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-pink-500 dark:hover:bg-pink-600 dark:focus:ring-pink-700">More Chuck!</button>
-        </div>
-        
+        </div>    
     </main>
   )
 }
