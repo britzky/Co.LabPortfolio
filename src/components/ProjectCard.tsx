ecbf902960e90db0b2a5261546234ca27816aefd
John <johnclapper89@gmail.com>
2023-09-29 13:41:17 -0400

    made projects section more responsive

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -11,58 +11,32 @@ interface ProjectCardProps {
 export const ProjectCard: React.FC<ProjectCardProps> = ({title, image, description, live, github, reverse}) => {
   return (
     <section className="py-5">
-        <div className="py-3 flex gap-12">
-            {reverse && 
-                <div className="flex flex-col gap-2">
-                    <img src={image} alt="Project image" className="rounded-xl max-w-lg" />
-                    <div className="flex justify-around">
-                        <a 
-                            href={live}
-                            rel="noreferrer"
-                            target="_blank"
-                            className="bg-sky-400 hover:bg-sky-600 text-white font-bold py-2 px-4 rounded"
-                        >
-                            See Live
-                        </a>
-                        <a
-                            rel="noreferrer"
-                            target="_blank"
-                            className="bg-amber-400 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded"
-                            href={github}
-                        >
-                            Source Code
-                        </a>
-                    </div>
-                </div>
-            }
+        <div className={`py-3 flex flex-col md:flex-row gap-4 md:gap-12 ${reverse ? 'md:flex-row-reverse' : ''}`}>
+            <div className="order-first md:order-none">
+                <img src={image} alt="Project image" className="rounded-xl max-w-lg" />
+            </div>
             <div className="max-w-md">
                 <h1 className="text-xl underline py-3">{title}:</h1>
                 <p>{description}</p>
-            </div>           
-            {!reverse &&
-                <div className="flex flex-col gap-2">
-                    <img src={image} alt="Project image" className="rounded-xl max-w-lg" />
-                    <div className="flex justify-around">
-                        <a 
-                            href={live}
-                            rel="noreferrer"
-                            target="_blank"
-                            className="bg-sky-400 hover:bg-sky-600 text-white font-bold py-2 px-4 rounded"
-                        >
-                            See Live
-                        </a>
-                        <a
-                            rel="noreferrer"
-                            target="_blank"
-                            className="bg-amber-400 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded"
-                            href={github}
-                        >
-                            Source Code
-                        </a>
-                    </div> 
+                <div className="flex gap-2 justify-around mt-4">
+                    <a 
+                        href={live}
+                        rel="noreferrer"
+                        target="_blank"
+                        className="bg-sky-400 hover:bg-sky-600 text-white font-bold py-2 px-4 rounded"
+                    >
+                        See Live
+                    </a>
+                    <a
+                        rel="noreferrer"
+                        target="_blank"
+                        className="bg-amber-400 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded"
+                        href={github}
+                    >
+                        Source Code
+                    </a>
                 </div>
-            }
-            
+            </div>                    
         </div>
     </section>
   )
