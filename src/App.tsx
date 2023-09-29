29d1126dc02aef695a8f1c7651c3f496748e8c7e
John <johnclapper89@gmail.com>
2023-09-29 12:48:36 -0400

    changed some colors

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <Router>
         <Header />
         <Home />
-        <div className="bg-sky-600 flex justify-around">
+        <div className="bg-pink-300 flex justify-around py-2">
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-plain-wordmark.svg" alt="python-icon" className="w-24" />
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original.svg" alt="flask-icon" className="w-24" />
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql-icon" className="w-24" />
@@ -17,7 +17,7 @@ function App() {
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt="html-icon" className="w-24" />         
         </div>
         <Projects />
-        <div className="bg-sky-600 flex justify-around">
+        <div className="bg-emerald-300 flex justify-around py-2">
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" alt="javascript-icon" className="w-24" />
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="react-icon" className="w-24" />
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-line.svg" alt="next-icon" className="w-24" />      
@@ -25,7 +25,7 @@ function App() {
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt="css-icon" className="w-24" />          
         </div>
         <Joke />
-        <div className="bg-sky-600 flex justify-around">  
+        <div className="bg-amber-300 flex justify-around py-2">  
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" alt="linux-icon" className="w-24" />
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-plain-wordmark.svg" alt="azure-icon" className="w-24" />
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain-wordmark.svg" alt="firebase-icon" className="w-24" />
