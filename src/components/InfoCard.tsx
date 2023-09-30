3b0e24b422534b6600d28471cc054f8d84038be7
John <johnclapper89@gmail.com>
2023-09-30 18:26:12 -0400

    cleaned dark mode from info card

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -9,7 +9,7 @@ interface InfoCardProps {
 
 export const InfoCard: React.FC<InfoCardProps> = ({title, description, isList, listItems}) => {
   return (
-    <section className="border rounded-lg p-7 bg-gray-50 dark:bg-gray-700">
+    <section className="border rounded-lg p-7 bg-gray-50">
         <h1 className="text-xl font-bold mb-3">{title}</h1>
         <p className="mb-5">{description}</p>
         {isList && listItems ? (
