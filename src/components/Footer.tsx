c9b39b7aaa89fde47ff81a5cfaf29af3ada42448
John <johnclapper89@gmail.com>
2023-09-30 18:25:44 -0400

    cleaned dark mode from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom"
 
 export const Footer = () => {
   return (
-    <footer className="bg-white shadow border-t-2 dark:border-t-1 dark:border-gray-900 dark:bg-gray-900">
+    <footer className="bg-white shadow border-t-2">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link to="/" className="hover:underline">John Clapper</Link></span>
-            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <span className="text-sm text-gray-500 sm:text-center">© 2023 <Link to="/" className="hover:underline">John Clapper</Link></span>
+            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
                 <li>
                     <a href="https://github.com/britzky" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Github</a>
                 </li>
